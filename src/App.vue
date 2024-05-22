76e7e39f9b9e9745b67f080d906104ecaf09a22c
liuye <2716235669@qq.com>
2024-05-22 11:15:43 +0800

    创建项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import HelloWorld from "@/components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+  <div class="test1">
+    111
+    <div class="test2">222</div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.test1 {
+  color: red;
+  .test2 {
+    color: aqua;
+  }
+}
+</style>
